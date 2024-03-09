4dfcc700ff14d0f09f900bfde68d8b7fcb46ec90
Michal Brzezinski <michal.brzezinski.off@gmail.com>
2024-03-09 14:57:11 +0100

    Initial commit

---
+++ library.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class library
+{
+	public:
+		addBook(Book b);
+		removeBook(Book b);
+		borrowBook(Member, isbn);
+		returnBook(Member, isbn);
+		displayBooks();
+
+	private:
+
+
+};
---
+++ main.cpp
@@ -0,0 +1,7 @@
+﻿#include <iostream>
+
+int main()
+{
+    std::cout << "Przykladowa forma zastosowania klasy\n"<<std::endl;
+    return 0;
+}
